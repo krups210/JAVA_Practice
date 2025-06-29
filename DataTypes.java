0c96bba754a8c288cd0ed64ae92afe03552565e5
krups210 <krupalgithub210@gmail.com>
2025-06-29 18:26:20 +0530

    DataType & Operators

---
+++ DataTypes.java
@@ -0,0 +1,43 @@
+package Core;
+
+public class DataTypes {
+	/*	 There are 2 types of DataTypes
+			1. Primitive
+			2. Non-Primitive
+			
+			1. Primitive
+				- Byte,short,int,long -> for numeric value
+				- float,double -> for decimal value
+				- boolean -> for True,False
+				- char -> for characters
+	*/
+	public static void main(String[] args) {
+		
+		byte b = -128;
+		System.out.println("Byte b = " + b);
+		
+		short s =12;
+		System.out.println("short s =" + s);
+		
+		int i =12;
+		System.out.println("Int I =" + i);
+		
+		long l = 12;
+		System.out.println("Long l = " + l);
+		
+		float f = 3.1224234324343f;
+		System.out.println("Float f =" + f);
+		
+		double d = 3.15454545;
+		System.out.println("double d = " + d);
+		
+		boolean bool = false;
+		System.out.println("boolean bool =" + bool);
+		
+		boolean bool1 = true;
+		System.out.println("boolean bool =" + bool1);
+				
+		char c= 'K';
+		System.out.println("character c =" + c);
+	}
+}
---
+++ Operators.java
@@ -0,0 +1,18 @@
+package Core;
+
+public class Operators {
+	/*
+		There are 8 types of Operators in Java.
+			1. Arithmetic Operators -> +, -, *, /, %
+			2. Unary Operators -> +, -, ++, --, !
+			3. Ternary Operators -> condition ? if true : if false
+			4. Assignment Operators -> =, +=, -=, *=, /=, %=
+			5. Relational Operators -> ==, !=, <, >, <=, >=
+			6. Logical Operators -> &&, ||, !
+			7. Bitwise Operators -> &, |, ^, ~
+			8. Shift Operators -> <<, >>, >>>
+			9. Instanceof Operators -> object instance of class/subclass/interface 
+
+ 
+	*/
+}
