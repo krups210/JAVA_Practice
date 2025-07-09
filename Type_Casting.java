f91a230577f227bb0b086634d6c991b375c0133f
krups210 <krupalgithub210@gmail.com>
2025-07-09 22:09:42 +0530

    Type Casting

---
+++ Type_Casting.java
@@ -0,0 +1,32 @@
+package Core;
+
+public class Type_Casting {
+	/*
+	 	- Type casting in Java is the process of converting a variable from one data type to another. 
+	 	  This is a fundamental concept in Java that allows for flexibility in data handling and operations 
+	 	  between different data types.
+	 	
+	 	- There are 2 types of Type Casting
+	 	 	1. Implicit / Automatic
+	 	 	2. Explicit / Manual
+	*/
+	public static void main(String[] args) {
+		
+		// Implicit
+		int myint = 9;
+		double d = myint;
+		System.out.println("Myint = " + myint);
+		System.out.println("Myint = " + d);
+		
+		// Explicit
+		float f1 = 6.345456f;
+		double d1 = 9.45;
+		int i1 = (int) d1;
+		int i2 = (int) f1;
+		System.out.println("Float f = " + f1);
+		System.out.println("double d = " + d1);
+		System.out.println("explicit value = " + i1);
+		System.out.println("explicit value = " + i2);
+		
+	}
+}
