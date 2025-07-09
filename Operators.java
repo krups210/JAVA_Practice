1ced741bd52c2208664c8d778353fa182edbea4d
krups210 <krupalgithub210@gmail.com>
2025-07-09 21:47:36 +0530

    Operator file update

--- Operators.java
+++ Operators.java
@@ -12,7 +12,32 @@ public class Operators {
 			7. Bitwise Operators -> &, |, ^, ~
 			8. Shift Operators -> <<, >>, >>>
 			9. Instanceof Operators -> object instance of class/subclass/interface 
-
- 
 	*/
+	
+	public static void main(String[] args) {
+		
+		//1. Arithmetic Operators
+		int a = 10;
+		int b = 3;
+		System.out.println("A = " + a);
+		System.out.println("B = " + b);
+		
+		
+		System.out.println("A + B = " + (a + b));
+		System.out.println("A - B = " + (a - b));
+		System.out.println("A * B = " + (a * b));
+		System.out.println("A / B = " + (a / b));
+		System.out.println("A % B = " + (a % b));
+		
+		//2. Unary Operator
+		
+		
+		int a2 = 12;
+		int b2 = 12;
+		System.out.println("A2 = " + (a2++));
+		System.out.println("A2 = " + (++a2));
+		System.out.println("B2 = " + (b2--));
+		System.out.println("B2 = " + (--b2));
+		
+	}
 }
