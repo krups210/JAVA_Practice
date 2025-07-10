53e2f08685496732341bbbe58925ab9de24aba80
krups210 <krupalgithub210@gmail.com>
2025-07-10 22:30:16 +0530

    Conditional statment final update

--- Conditional_Statements.java
+++ Conditional_Statements.java
@@ -50,7 +50,7 @@ public class Conditional_Statements {
 		else {
 			System.out.println("Age is less than 18");
 		}*/
-		
+		/*
 		int mynum = 10;
 		if(mynum > 0 & mynum <= 10) {
 			System.out.println("my number is positive");
@@ -60,7 +60,47 @@ public class Conditional_Statements {
 			System.out.println("mu number is negative");
 		}else {
 			System.out.println("value is zero");
+		}*/
+		
+		// else if ladder
+		/*int mark = 101;
+		if(mark < 35) {
+			System.out.println("Fail");
+		}else if(mark >=35 && mark <=60) {
+			System.out.println("D Grade");
+		}else if(mark >=61 && mark <=70) {
+			System.out.println("C Grade");
+		}else if(mark >=71 && mark <=80) {
+			System.out.println("B Grade");
+		}else if(mark >=81 && mark <= 100) {
+			System.out.println("A Grade");
+		}else {
+			System.out.println("Invali Input");
+		}*/
+		
+		// switch case
+		System.out.println("1.Gujarati 2.Hindi 3.English");
+		System.out.println();
+		int choice = 4;
+		switch (choice) {
+		case 1: 
+			System.out.println("You selected " + choice + "st choice:: Gujarati");
+			break;
+
+		case 2:
+			System.out.println("You selected " + choice + "nd choice:: Hindi");
+			break;
+			
+		case 3:
+			System.out.println("You selected " + choice + "rd choice:: English");
+			break;
+			
+		default: 
+			System.out.println("Invalid Input choice:: " + choice);
+			break;
 		}
+		
+			
 	}
 	
 }
